f5d393c7354d97ee999637bf870b4159cafdd6ae
its.plexz <MO7.ADE1.37@GMAIL.COM>
2026-08-28 01:43:01 +0300

    chore: remove scratch file

--- scratch/test_settings.js
+++
@@ -1,9 +0,0 @@
-import { getMergedSettings } from '../server/routes/store.js';
-
-async function test() {
-  const s = await getMergedSettings();
-  console.log('Current Merged Store Name:', s.store_name);
-  console.log('Current Merged Tagline:', s.tagline);
-}
-
-test();
